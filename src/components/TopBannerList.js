7d898619052499952e389f4b31992a6680a49365
asifhjahid <dhrubo8211diu@gmail.com>
2022-03-09 23:34:31 +0600

    updated top banner list products

--- src/components/TopBannerList.js
+++ src/components/TopBannerList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import TopBannerListStyle from '../styles/TopBannerList.styles'
 
 export default function TopBannerList() {
@@ -7,15 +8,25 @@ export default function TopBannerList() {
     
             <div class="global_category_tab_btn_area">
               <div class="global_category_tab_btn_wrapper">
+              
                 <div class="single_global_catergory_btn">
+                <Link to='/freshproduct'  style={{textDecoration:"none"}}>
                   <a class="globalBtn_link" href="#">Fresh</a>
+                  </Link>
                 </div>
+                              
                 <div class="single_global_catergory_btn">
+                <Link to='/regularproduct'  style={{textDecoration:"none"}}>
                   <a class="globalBtn_link" href="#">Regular</a>
+                  </Link>
                 </div>
+                            
                 <div class="single_global_catergory_btn">
+                <Link to='/preorder'  style={{textDecoration:"none"}}>
                   <a class="globalBtn_link" href="#">Preorder</a>
+                    </Link>
                 </div>
+              
               </div>
             </div>
         
